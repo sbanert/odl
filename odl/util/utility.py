75339d3e4ffd7af063a7886c89e300e725e6d9f9
Holger Kohr <h.kohr@cwi.nl>
2017-08-11 11:34:30 +0200

    MAINT: small fix to npy_printoptions

--- odl/util/utility.py
+++ odl/util/utility.py
@@ -76,7 +76,7 @@ class npy_printoptions(object):
     def __enter__(self):
         np.set_printoptions(**self.new_opts)
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, *exc_info):
         np.set_printoptions(**self.orig_opts)
 
 
